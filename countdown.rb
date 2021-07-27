bf5d2506a12e91d453e9c1e7b0d9ee22afcfc768
Caitlin Raby <caitlinraby@gmail.com>
2021-07-27 16:31:20 -0500

    commit tree f52771aea86eb5a201eb1df2c775a379707eaa9b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    puts "#{number} SECOND(S)"
+    until number == 1
+        number -= 1
+        puts "-#{number} SECOND(S)"
+    end
+    puts "HAPPY NEW YEARE!"
 end
